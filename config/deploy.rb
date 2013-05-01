eaa3799a34a600ce5238e884f84cb86ed2c203fb
David Celis <me@davidcel.is>
2013-05-01 13:47:03 -0700

    Fix booting of clockworkd
    Signed-off-by: David Celis <me@davidcel.is>

--- config/deploy.rb
+++ config/deploy.rb
@@ -81,15 +81,15 @@ after "deploy:finalize_update", "deploy:symlink_config"
 namespace :clockwork do
   desc "Start clockwork"
   task :start, :roles => [:app] do
-    run "cd #{current_path} && #{rails_env} bundle exec clockworkd -c #{current_path}/config/clock.rb --pid-dir #{shared_path}/pids --log-dir #{shared_path}/log start"
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec clockworkd -c #{current_path}/config/clock.rb --pid-dir #{shared_path}/pids --log --log-dir #{shared_path}/log start"
   end
 
   task :stop, :roles => [:app] do
-    run "cd #{current_path} && #{rails_env} bundle exec clockworkd stop"
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec clockworkd -c #{current_path}/config/clock.rb --pid-dir #{shared_path}/pids --log --log-dir #{shared_path}/log stop"
   end
 
   task :restart, :roles => [:app] do
-    run "cd #{current_path} && #{rails_env} bundle exec clockworkd -c #{current_path}/config/clock.rb --pid-dir #{shared_path}/pids --log-dir #{shared_path}/log restart"
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec clockworkd -c #{current_path}/config/clock.rb --pid-dir #{shared_path}/pids --log --log-dir #{shared_path}/log restart"
   end
 end
 
